e9d7df531dd44fe3e1fc3f5f23698a63aa3f0ceb
evgenshkuropat <evgenshkuropat@gmail.com>
2026-08-17 00:20:24 +0200

    Increase Sauto parser page limit

--- src/main/java/com/yourapp/carbot/service/SautoParser.java
+++ src/main/java/com/yourapp/carbot/service/SautoParser.java
@@ -29,7 +29,7 @@ public class SautoParser implements CarSourceParser {
     private static final Logger log = LoggerFactory.getLogger(SautoParser.class);
 
     private static final String BASE_LIST_URL = "https://www.sauto.cz/inzerce/osobni?cena-od=30000&razeni=od-nejlevnejsich";
-    private static final int MAX_LIST_PAGES = 8;
+    private static final int MAX_LIST_PAGES = 10;
     private static final int REQUEST_TIMEOUT_MS = 15_000;
 
     private static final int CURRENT_YEAR = Year.now().getValue();
